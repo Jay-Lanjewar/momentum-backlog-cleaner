849b25808ee68eb47a3ec85d5fa3efc6259dd670
Jay Lanjewar <jaylanjewar619@gmail.com>
2026-07-27 19:10:28 +0530

    fix: disable asyncpg statement cache for alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -61,6 +61,9 @@ async def run_async_migrations() -> None:
         configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
+        connect_args={
+            "statement_cache_size": 0,
+        },
     )
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
